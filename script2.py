df62e6bd911734bcc79c60a45265e23521dec758
Cmars3221 <Cmars3221@gmail.com>
2016-10-10 00:28:34 -0400

    First commit, multiple test files

---
+++ script2.py
@@ -0,0 +1,30 @@
+import folium
+import pandas
+
+df = pandas.read_csv("Volcanoes-USA.txt")
+
+#map starting point.
+map=folium.Map(location=[df['LAT'].mean(),df['LON'].mean()],zoom_start=5,tiles="Stamen Terrain")
+
+#marker color elector
+def color(elev):
+    minimum = int(min(df['ELEV']))
+    maximum  = int(max(df['ELEV']))
+    step = int((maximum-minimum)/3)
+
+    if elev in range(minimum,step+minimum):
+        col = 'green'
+    elif elev in range(minimum+step,minimum+step*2):
+        col = 'orange'
+    else:
+        col = 'red'
+
+
+for lat,lon,name,elev in zip(df['LAT'],df['LON'],df['NAME'],df['ELEV']):
+    map.add_child(folium.Marker(location=[lat,lon],popup=name, icon=folium.Icon(color(elev),icon_color='black')))
+
+
+
+map.add_child(folium.GeoJson(data=open('worldpop.json'),name='World Population'))
+
+map.save(outfile='test5.html')
